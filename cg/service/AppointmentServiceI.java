59d435f3e89f61ad53946603c55cab1b680a9048
SarayuBobbili <37653704+SarayuBobbili@users.noreply.github.com>
2020-04-29 17:58:34 +0530

    Update AppointmentServiceI.java

--- cg/service/AppointmentServiceI.java
+++ cg/service/AppointmentServiceI.java
@@ -1,22 +1,28 @@
 package com.cg.service;
 
-import java.math.BigInteger;
-import java.time.LocalDateTime;
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
 import com.cg.entity.Appointment;
 import com.cg.entity.DiagnosticCenter;
 import com.cg.entity.Test;
 import com.cg.entity.User;
+import com.cg.exception.ResourceNotFoundException;
 
 public interface AppointmentServiceI {
 	
-	public int makeAppointment(User user,DiagnosticCenter center, Test test ,LocalDateTime datetime);
-	public List viewAppointmentDetails();
-	public Test findByTestName(DiagnosticCenter diagnosticCenter, String testName);
-	public DiagnosticCenter findByCenterName(String centerName);
-	public User findByUserId(int userId);
+	  public String addCenter(DiagnosticCenter diagnosticCenter);
+		
+			public DiagnosticCenter findByCenterName(String centerName) throws ResourceNotFoundException;
+			
+			public Test findByTestName(DiagnosticCenter diagnosticCenter, String testName) throws ResourceNotFoundException;
+			
+			public boolean isCenterNameExist(String centerName);
+			
+			public String createUser(User user);
 
+			public User findByUserId(int userId);
+			
+			public String makeAppointment(Appointment appointment);
+			
+			public Appointment findByAppointmentId(int appointmentId);
+
+			public String viewAppointment(int userId);
 }
