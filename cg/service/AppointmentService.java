00d144055eb5da01f928de990f2c6d1a21788a3b
SarayuBobbili <37653704+SarayuBobbili@users.noreply.github.com>
2020-04-29 17:59:12 +0530

    Update AppointmentService.java

--- cg/service/AppointmentService.java
+++ cg/service/AppointmentService.java
@@ -1,49 +1,96 @@
 package com.cg.service;
 
-import java.math.BigInteger;
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cg.dao.AppointmentDao;
 import com.cg.dao.AppointmentDaoI;
 import com.cg.entity.Appointment;
 import com.cg.entity.DiagnosticCenter;
 import com.cg.entity.Test;
 import com.cg.entity.User;
+import com.cg.exception.ResourceNotFoundException;
 
 @Service
 public class AppointmentService implements AppointmentServiceI {
 	
 	@Autowired
-	AppointmentDaoI appdao;
-
+	AppointmentDao appoitnmentDao;
 
 	@Override
-	public int makeAppointment(User user, DiagnosticCenter center, Test test, LocalDateTime datetime) {
-		return appdao.makeAppointment(user, center, test, datetime);
+	public String addCenter(DiagnosticCenter diagnosticCenter) 
+	{
+		return appoitnmentDao.addCenter(diagnosticCenter);
+	}
+
+	@Override
+	public DiagnosticCenter findByCenterName(String centerName) throws ResourceNotFoundException 
+	{
+		List<DiagnosticCenter> listOfDiagnosticCenters = appoitnmentDao.listOfDiagnosticCenters();
+		boolean isDiagnosticCentPresent = listOfDiagnosticCenters.stream().filter(center->center.getCenterName().equalsIgnoreCase(centerName)).findFirst().isPresent();
+		if(!isDiagnosticCentPresent)
+			throw new ResourceNotFoundException("Not found center "+centerName);		
+	    DiagnosticCenter diagnosticCenter = listOfDiagnosticCenters.stream().filter(center->center.getCenterName().equalsIgnoreCase(centerName)).findFirst().get();
+	    return diagnosticCenter;
+	}
+
+	@Override
+	public Test findByTestName(DiagnosticCenter diagnosticCenter, String testName) throws ResourceNotFoundException 
+	{
+		boolean isTestPresent = diagnosticCenter.getListOfTests().stream().filter(t->testName.equalsIgnoreCase(t.getTestName())).findFirst().isPresent();
+		if(!isTestPresent)
+			throw new ResourceNotFoundException(testName+" test does not exist in the "+diagnosticCenter.getCenterName()+" center");
+		Test test = diagnosticCenter.getListOfTests().stream().filter(t->testName.equalsIgnoreCase(t.getTestName())).findFirst().get();
+		return test;
+	}
+
+	@Override
+	public boolean isCenterNameExist(String centerName)
+	{
+		List<DiagnosticCenter> listOfDiagnosticCenters = appoitnmentDao.listOfDiagnosticCenters();
+		if(!listOfDiagnosticCenters.stream().filter(center->center.getCenterName().equalsIgnoreCase(centerName)).findFirst().isPresent())
+			return false;
+		return true;
 	}
 	
 	@Override
-	public List viewAppointmentDetails() {
-		return appdao.viewAppointmentDetails();
+	public String createUser(User user)
+	{
+		return appoitnmentDao.createUser(user);
 	}
-
 	
 	@Override
-	public Test findByTestName(DiagnosticCenter diagnosticCenter, String testName) {
-		
-		return appdao.findByTestName(diagnosticCenter, testName);
+	public User findByUserId(int userId)
+	{
+		return appoitnmentDao.findByUserId(userId);
 	}
-
-	public DiagnosticCenter findByCenterName(String centerName) {
-		
-		return appdao.findByCenterName(centerName);
+	
+	@Override
+	public String makeAppointment(Appointment appointment)
+	{
+		return appoitnmentDao.makeAppointment(appointment);
 	}
-
-	public User findByUserId(int userId) {
-		
-		return appdao.findByUserId(userId);
+	
+	@Override
+	public Appointment findByAppointmentId(int appointmentId)
+	{
+		return appoitnmentDao.findByAppointmentId(appointmentId);
+	}
+	
+	@Override
+	public String viewAppointment(int userId) 
+	{
+		User user = findByUserId(userId);
+		int appointmentId = user.getAppointment().getAppointmentId();
+		Appointment appointment = findByAppointmentId(appointmentId);
+		String appointmentInfo = "Appointment Id : "+appointment.getAppointmentId()+
+				                 "\nCenter Name    : "+appointment.getCenter().getCenterName()+
+				                 "\nTest Name      : "+appointment.getTest().getTestName()+
+				                 "\nAddress        :"+ appointment.getCenter().getAddress()+
+				                 "\nContact Number : "+appointment.getCenter().getContactNumber()+
+				                 "\nApproval Staus : "+appointment.isApproved();
+		return appointmentInfo;
 	}
 }
