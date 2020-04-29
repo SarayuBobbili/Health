fad699105ce3d40a5e45f4af140640a30c76cecd
SarayuBobbili <37653704+SarayuBobbili@users.noreply.github.com>
2020-04-29 17:50:27 +0530

    Update AppointmentController.java

--- cg/controller/AppointmentController.java
+++ cg/controller/AppointmentController.java
@@ -1,39 +1,62 @@
 package com.cg.controller;
 
-import java.math.BigInteger;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cg.entity.Appointment;
 import com.cg.entity.DiagnosticCenter;
 import com.cg.entity.Test;
 import com.cg.entity.User;
+import com.cg.exception.BadRequestException;
+import com.cg.exception.ResourceNotFoundException;
 import com.cg.service.AppointmentService;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
-public class AppointmentController {
-	
+public class AppointmentController 
+{
 	@Autowired
-	AppointmentService appointmentservice;
-	
+	AppointmentService appointmentService;
 
-	@PostMapping(value="HealthCareSystem/users/make-appointment/{userId}/{centerName}/{testName}/{dateTimeStr}")
-	public int makeAppointment(@PathVariable int userId,@PathVariable String centerName ,@PathVariable String testName,@PathVariable String dateTimeStr)
+	@PostMapping(value="HealthCareSystem/add-center", consumes = {"application/json"})
+	public String addCenter(@Valid @RequestBody DiagnosticCenter diagnosticCenter) throws BadRequestException
 	{
-		User user =appointmentservice.findByUserId(userId);
-		DiagnosticCenter diagnosticCenter =appointmentservice.findByCenterName(centerName);
-		Test test=appointmentservice.findByTestName(diagnosticCenter, testName);
-		
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-		LocalDateTime dateTime = LocalDateTime.parse(dateTimeStr, formatter);
-
-		//LocalDateTime datetime= LocalDateTime.parse(dateTime);
-		appointmentservice.makeAppointment(user, diagnosticCenter, test, dateTime);
-		return 1;
+		if(appointmentService.isCenterNameExist(diagnosticCenter.getCenterName()))
+		{
+			throw new BadRequestException(diagnosticCenter.getCenterName()+" center name already exits");
+		}
+		return appointmentService.addCenter(diagnosticCenter);
+	}
+	
+	@PostMapping(value="HealthCareSystem/make-appointment/{centerName}/{testName}/{userId}", consumes = {"application/json"})
+	public String makeAppointment(@PathVariable String centerName, @PathVariable String testName, @PathVariable int userId, @RequestBody Appointment appointment) throws ResourceNotFoundException
+	{
+		DiagnosticCenter diagnosticCenter = appointmentService.findByCenterName(centerName);
+		Test test = appointmentService.findByTestName(diagnosticCenter, testName);
+		User user = appointmentService.findByUserId(userId);
+		appointment.setCenter(diagnosticCenter);
+		appointment.setTest(test);
+		appointment.setUser(user);
+		return appointmentService.makeAppointment(appointment);
 	} 
-
+	
+	@PostMapping(value="HealthCareSystem/create-user", consumes = {"application/json"})
+	public String createUser(@Valid @RequestBody User user)
+	{
+		return appointmentService.createUser(user);
+	}
+	
+	@GetMapping(value="HealthCareSystem/View Appointment/{userId}")
+	public String viewAppointment(@PathVariable int userId)
+	{
+		return appointmentService.viewAppointment(userId);	
+	}
+}
